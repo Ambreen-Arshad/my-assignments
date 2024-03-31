911255301e7cbab1d5e01041fedbcd21f3a25109
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-31 14:13:57 +0500

    first commit

---
+++ Q_2/index.js
@@ -0,0 +1,7 @@
+// Date 11-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 2 in typescript.
+// Personal Message: Store a person’s name in a variable, and print a message to that person.\n
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let personName = "Eric";
+console.log(`" Hello ${personName}, would you like to learn some Python today?"`);
+export {};
