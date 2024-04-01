9b0f8da00bff0f8fb1602a2b38e17e6a72541096
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 03:48:46 +0500

    Q_7 commit

---
+++ Q_7/index.ts
@@ -0,0 +1,19 @@
+// Date 12-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 07-08 in typescript assignment.
+
+// #07-Number Eight: Write addition, subtraction, multiplication, and division operations that each result in\n
+// the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// #08-You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log(4 + 4 ) // (addition)
+
+console.log(10 - 2) // (subtraction)
+
+console.log(4 * 2 ) // (multiplication)
+
+console.log(16 /2)  // (division)
