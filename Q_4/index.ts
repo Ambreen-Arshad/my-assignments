b46f84f13dd13c0e7784cc376a630ccacfd86adc
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 03:21:49 +0500

    Q_4 commit

---
+++ Q_4/index.ts
@@ -0,0 +1,7 @@
+// Date 11-3-2024
+// This is "Ambreen",and you are watching the problem no 4 intypescript assignment.
+
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.\n
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log('Albert Einstein once said, "A person who never made a mistake never tried anything new."'); 
