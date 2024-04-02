dde879bca4a5f663b395795cad5249cb6b81673c
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 05:52:17 +0500

    Q_10

---
+++ Q_10/index.ts
@@ -0,0 +1,12 @@
+// Date 12-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 10 in typescript assignment.
+
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.\n
+// If you don’t have anything specific to write because your programs are too simple at this point, \n
+//just add your name and the current date at the top of each program file.\n
+// Then write one sentence describing what the program does.
+
+
+// Now we describe the code in this program we will write something for my loving SON,
+
+console.log("My cutest son`s name is    \n\tMUHAMMAD UMAR KHAN\t\n    he is so inocent,so lovely and so...so...speacial for me, LOVE you beta!");
