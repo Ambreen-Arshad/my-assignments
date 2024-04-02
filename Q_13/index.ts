2e94dc9db2fa7e566aa74551cce54455e6017a2c
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 11:36:18 +0500

    Q_13

---
+++ Q_13/index.ts
@@ -0,0 +1,12 @@
+// Date 12-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 13 in typescript assignment.
+
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that\n
+// stores several examples. Use your list to print a series of statements about these items,\n
+// such as “I would like to own a Honda motorcycle.”
+
+let  transportation : string[] = ['motorcycle','car','train','cycle','airplane'];
+for(let i=0; i <  transportation.length; i ++){
+
+    console.log('I would like to own a ' +   transportation [i] );
+}
