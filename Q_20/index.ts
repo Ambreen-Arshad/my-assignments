faa3f5664ea81d39a10bf841a72c30ac8f24073d
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 12:49:00 +0500

    Q_20 commit

---
+++ Q_20/index.ts
@@ -0,0 +1,19 @@
+// Date 14-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 20 in typescript assignment.
+
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers,\n
+// countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+//Think of something you could store in a array.
+
+let itsms : string[] = ['Pakistan','Karachi','Urdu'];
+
+//Write a program that creates a list containing these items.
+
+let items2 : string[] = [];
+
+items2.push('Pakistan');
+items2.push('Karachi');
+items2.push('Urdu');
+
+console.log(items2);
