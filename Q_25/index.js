89ccc97cf227dbde784b10ac1c5daba37c7e4017
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 13:18:28 +0500

    Q_25 commit

---
+++ Q_25/index.js
@@ -0,0 +1,16 @@
+// Date 17-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 25 in typescript assignment.
+// Create a variable called alien_color
+let alien_color = 'green';
+//• Write an if statement to test whether the alien’s color is green. If it is,\n
+// print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points !");
+}
+//• Write one version of this program that passes the if test and another that fail.
+alien_color = 'Yellow';
+// (The version that fails will have no output.)
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points !");
+}
+export {};
