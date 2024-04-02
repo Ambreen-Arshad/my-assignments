850b486923bf91d03b65c05b12d9a41a237d5a2a
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 11:14:54 +0500

    Q_11 commit

---
+++ Q_11/index.ts
@@ -0,0 +1,13 @@
+// Date 12-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 11 in typescript assignment.
+
+// Names: Store the names of a few of your friends in a array called names.\n
+// Print each person’s name by accessing each element in the list, one at a time.
+
+let names : string[] = ['Arshad','Alisha','Saim','Alishba','Sarim','Umar'];
+
+for(let i = 0; i < names.length; i++ ){
+    
+    console.log(names[i]);
+    
+}
