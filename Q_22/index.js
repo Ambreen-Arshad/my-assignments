3e5f37e431b8f9fe5d2bfa341ff1da608199fc9a
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-02 13:01:17 +0500

    Q_22 commit

---
+++ Q_22/index.js
@@ -0,0 +1,13 @@
+// Date 15-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 22 in typescript assignment.
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.\n
+// Change an index in one of your programs to produce an index error.\n
+// Make sure you correct the error before closing the program.
+// creating array
+let newArray = ['Pakistan', 'Sindh', 2, 'Karachi', 100];
+// Accessing array by index (print the array)
+console.log(newArray[6]);
+// Make sure you correct the error before closing the program.
+// correct error
+console.log(newArray);
+export {};
