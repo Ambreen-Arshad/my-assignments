47a0d296a184f4bde94043bf0f7d9ff7a280966f
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-03 14:58:25 +0500

    Q_38 commit

---
+++ Q_38/src/index.ts
@@ -0,0 +1,16 @@
+// Date 19-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 38 in typescript assignment.
+
+// Cities: Write a function called describe_city() that accepts the name of a city and its country. \n
+// The function should print a simple sentence, such as Karachi is in Pakistan. \n
+// Give the parameter for the country a default value. Call your function for three different cities,\n
+// at least one of which is not in the default country.
+
+function describe_city(city : string , country : string = 'Default Country.'){
+
+    console.log(`${city} is in ${ country}`);
+
+}
+describe_city('Karachi','Pakistan');
+describe_city('Tokyo','Japan');
+describe_city('Paris');
