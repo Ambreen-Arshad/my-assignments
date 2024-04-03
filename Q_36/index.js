02df878f7331dd52c42d8d3e3626309255421add
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-03 13:45:21 +0500

    Q_36 commit

---
+++ Q_36/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Date 19-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 36 in typescript assignment.
+Object.defineProperty(exports, "__esModule", { value: true });
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that \n
+// should be printed on the shirt. \n
+// The function should print a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function.
+function make_shirt(size, text) {
+    console.log(`creating a ${size} shirt with the messag : ${text} `);
+}
+make_shirt('large', 'Hello World');
---
+++ Q_36/index.ts
@@ -0,0 +1,15 @@
+// Date 19-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 36 in typescript assignment.
+
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that \n
+// should be printed on the shirt. \n
+// The function should print a sentence summarizing the size of the shirt and the message printed on it.
+
+
+// Call the function.
+
+function make_shirt(size : string , text: string){
+
+    console.log(`creating a ${size} shirt with the messag : ${text} `)
+}
+make_shirt('large','Hello World')
