05b0b508613ffd15ab72c47a5056cf43999ec2b4
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-03 16:14:42 +0500

    Q_44 commit

---
+++ Q_44/src/index.ts
@@ -0,0 +1,23 @@
+// Date 26-3-2024
+// This is "Ambreen",and you are watching the solution of the problem no 44 in typescript assignment.
+
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. \n
+// The function should have one parameter that collects as many items as the function call provides, \n
+// and it should print a summary of the sandwich that is being ordered. \n
+// Call the function three times, using a different number of arguments each time.
+
+function makeSandwich(items : string[]){
+
+    console.log('\n\nMake your Sandwich with :  ');
+
+    items.forEach(Element => console.log("-  " + Element));
+
+    console.log('Enjoy your Sandwich! ');
+
+}
+
+makeSandwich(['Ham','Cheese','Lettuce']);
+
+makeSandwich(['Turkey','Bacon']);
+
+makeSandwich(['Peanut butter','Jelly']);
